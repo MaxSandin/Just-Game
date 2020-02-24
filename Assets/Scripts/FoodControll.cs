c3ac384593801e9f5037f2d13ddee40cdacf5054
ilzov <illia.kozhara@gmail.com>
2020-02-24 19:20:34 +0200

    Start project structure has been modified. Few control (MainCharacter, MainCharacter, Camera, Daylight, Food) scripts has been added.

---
+++ Assets/Scripts/FoodControll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodControll : MonoBehaviour
+    //Food (some components or details in future) controll script
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
