706d723d38ea793f24b83aa3abc89c367f3b35fa
ilzov <illia.kozhara@gmail.com>
2020-04-12 12:27:52 +0300

    Добавил скрипты для перехода к игре и выхода.

---
+++ Assets/MaiMenuScrpt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MaiMenuScrpt : MonoBehaviour
+{
+    public void playNewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void continueGame()
+    {
+        // TooDoo>> Ad continue script after saving method has been added.
+        // It is play new game functionality now!!
+        playNewGame();
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
