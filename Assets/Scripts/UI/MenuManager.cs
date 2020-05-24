c3b993b886f75d38e7c237583771afcc49027833
Nikolay Popov <popovnv94@gmail.com>
2020-05-24 20:41:01 +0300

    Добавил панель меню в игре

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void ShowMenuPanel(GameObject obj)
+    {
+        obj.SetActive(true);
+        SetGamePause(true);
+    }
+
+    public void HideMenuPanel(GameObject obj)
+    {
+        obj.SetActive(false);
+        SetGamePause(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    void SetGamePause(bool pause)
+    {
+        if (pause)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }    
+}
