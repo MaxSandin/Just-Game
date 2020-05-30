2d39caa478b9bd304c288633504c3228f078eeff
Nikolay Popov <popovnv94@gmail.com>
2020-05-30 11:16:19 +0300

    добавил лог

---
+++ Assets/Scripts/UI/LogManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogManager : MonoBehaviour
+{
+    [SerializeField] private Text text;
+    [SerializeField] private int logLimit = 5;
+
+    private string myLog;
+    private Queue myLogQueue = new Queue();
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        myLog = logString;
+        string newString = "[" + type + "] : " + myLog + "\n";
+        if (myLogQueue.Count >= logLimit)
+            myLogQueue.Dequeue();
+        myLogQueue.Enqueue(newString);
+
+        if (type == LogType.Exception)
+        {
+            newString = "\n" + stackTrace;
+            myLogQueue.Enqueue(newString);
+        }
+        myLog = string.Empty;
+
+        foreach (string mylog in myLogQueue)
+        {
+            myLog += mylog;
+        }
+    }
+
+    void OnGUI()
+    {
+        text.text = myLog;
+    }
+}
